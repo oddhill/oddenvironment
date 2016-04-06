dd6bbd68fa41ad7d92c44ae0479c329fe27ce6e6
Jens Lind <jens.lind@oddhill.se>
2016-04-06 11:23:10 +0200

    mysql php configs

--- site.pp
+++ site.pp
@@ -56,3 +56,18 @@ service { 'homebrew.mxcl.dnsmasq':
   ensure  => 'running',
   require => File['/Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist']
 }
+
+$php_mysql_socket = '/private/tmp/mysql.sock'
+
+file { '/usr/local/etc/php/5.5/php.ini':
+  content => template("${templates}/php/php.ini.erb")
+}
+
+file { '/Library/LaunchDaemons/homebrew.mxcl.mysql.plist':
+  content => template("${templates}/mysql/homebrew.mxcl.mysql.plist.erb")
+}
+
+service { 'homebrew.mxcl.mysql':
+  ensure => 'running',
+  require => File['/Library/LaunchDaemons/homebrew.mxcl.mysql.plist']
+}
