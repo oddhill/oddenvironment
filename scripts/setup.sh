9c854f4a8cb10bd82d57fdc811a9480c43816e17
Jens Lind <jens.lind@oddhill.se>
2016-04-06 22:12:13 +0200

    Install bundler on setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 brew --version || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+sudo gem install bundler
+
 sudo mkdir -p /etc/facter/facts.d
 sudo touch /etc/facter/facts.d/env.txt
 echo home=$HOME | sudo tee -a /etc/facter/facts.d/env.txt
