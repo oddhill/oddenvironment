b3e7781828cbad0b811e42c554d28df1f53e4a4d
Jens Lind <jens.lind@oddhill.se>
2016-04-15 11:43:38 +0200

    Set NODE_PATH env var #8

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,3 +3,4 @@ alias bergerac="$BERGERAC_DIR/scripts/bergerac.sh"
 PATH=$BERGERAC_DIR/node_modules/.bin:$BERGERAC_DIR/vendor/bin:$PATH
 export NVM_DIR="$HOME/.nvm"
 source "$(brew --prefix nvm)/nvm.sh"
+export NODE_PATH=`npm root -g`
